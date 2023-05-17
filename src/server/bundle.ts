dc4807410130078b670cfb6433f1ed4a993176a1
gimenes <tlgimenes@gmail.com>
2023-05-17 10:06:24 -0300

    prebuild bundle

--- src/server/bundle.ts
+++ src/server/bundle.ts
@@ -1,151 +1,145 @@
-import { BuildOptions } from "https://deno.land/x/esbuild@v0.17.11/mod.js";
+import {
+  BuildOptions,
+  BuildResult,
+} from "https://deno.land/x/esbuild@v0.17.11/mod.js";
 import { BUILD_ID } from "./constants.ts";
-import { denoPlugin, esbuild, toFileUrl } from "./deps.ts";
+import { denoPlugin, esbuild, join, toFileUrl } from "./deps.ts";
 import { Island, Plugin } from "./types.ts";
+import { BlobStorage, fsStorage, inMemoryStorage } from "./storage.ts";
 
 export interface JSXConfig {
   jsx: "react" | "react-jsx";
   jsxImportSource?: string;
 }
 
-let esbuildInitialized: boolean | Promise<void> = false;
-async function ensureEsbuildInitialized() {
-  if (esbuildInitialized === false) {
-    // deno-lint-ignore no-deprecated-deno-api
-    if (Deno.run === undefined) {
-      const wasmURL = new URL("./esbuild_v0.17.11.wasm", import.meta.url).href;
-      esbuildInitialized = fetch(wasmURL).then(async (r) => {
-        const resp = new Response(r.body, {
-          headers: { "Content-Type": "application/wasm" },
-        });
-        const wasmModule = await WebAssembly.compileStreaming(resp);
-        await esbuild.initialize({
-          wasmModule,
-          worker: false,
-        });
-      });
-    } else {
-      esbuild.initialize({});
-    }
-    await esbuildInitialized;
-    esbuildInitialized = true;
-  } else if (esbuildInitialized instanceof Promise) {
-    await esbuildInitialized;
+let esBuildInitPromise: Promise<void> | undefined = undefined;
+const ensureEsbuildInitialized = () => {
+  if (!esBuildInitPromise) {
+    const onDenoDeploy = Deno.run === undefined;
+
+    esBuildInitPromise = onDenoDeploy
+      ? fetch(new URL("./esbuild_v0.17.11.wasm", import.meta.url))
+        .then((r) =>
+          WebAssembly.compileStreaming(
+            new Response(r.body, {
+              headers: { "Content-Type": "application/wasm" },
+            }),
+          )
+        )
+        .then((wasmModule) => esbuild.initialize({ wasmModule, worker: false }))
+      : esbuild.initialize({});
   }
-}
+
+  return esBuildInitPromise;
+};
 
 const JSX_RUNTIME_MODE = {
   "react": "transform",
   "react-jsx": "automatic",
 } as const;
 
-export class Bundler {
-  #importMapURL: URL;
-  #jsxConfig: JSXConfig;
-  #islands: Island[];
-  #plugins: Plugin[];
-  #cache: Map<string, Uint8Array> | Promise<void> | undefined = undefined;
-  #dev: boolean;
+interface Options {
+  importMapURL: URL;
+  jsxConfig: JSXConfig;
+  islands: Island[];
+  plugins: Plugin[];
+  dev: boolean;
+  absWorkingDir: string;
+}
 
-  constructor(
-    islands: Island[],
-    plugins: Plugin[],
-    importMapURL: URL,
-    jsxConfig: JSXConfig,
-    dev: boolean,
-  ) {
-    this.#islands = islands;
-    this.#plugins = plugins;
-    this.#importMapURL = importMapURL;
-    this.#jsxConfig = jsxConfig;
-    this.#dev = dev;
+const bundle = async (
+  { importMapURL, dev, islands, plugins, jsxConfig, absWorkingDir }: Options,
+) => {
+  const entryPoints: Record<string, string> = {
+    main: dev
+      ? new URL("../../src/runtime/main_dev.ts", import.meta.url).href
+      : new URL("../../src/runtime/main.ts", import.meta.url).href,
+  };
+
+  for (const island of islands) {
+    entryPoints[`island-${island.id}`] = island.url;
   }
 
-  async bundle() {
-    const entryPoints: Record<string, string> = {
-      main: this.#dev
-        ? new URL("../../src/runtime/main_dev.ts", import.meta.url).href
-        : new URL("../../src/runtime/main.ts", import.meta.url).href,
-    };
-
-    for (const island of this.#islands) {
-      entryPoints[`island-${island.id}`] = island.url;
+  for (const plugin of plugins) {
+    for (const [name, url] of Object.entries(plugin.entrypoints ?? {})) {
+      entryPoints[`plugin-${plugin.name}-${name}`] = url;
     }
+  }
 
-    for (const plugin of this.#plugins) {
-      for (const [name, url] of Object.entries(plugin.entrypoints ?? {})) {
-        entryPoints[`plugin-${plugin.name}-${name}`] = url;
-      }
-    }
+  await ensureEsbuildInitialized();
+  // In dev-mode we skip identifier minification to be able to show proper
+  // component names in Preact DevTools instead of single characters.
+  const minifyOptions: Partial<BuildOptions> = dev
+    ? { minifyIdentifiers: false, minifySyntax: true, minifyWhitespace: true }
+    : { minify: true };
+  const bundle = await esbuild.build({
+    bundle: true,
+    define: { __FRSH_BUILD_ID: `"${BUILD_ID}"` },
+    entryPoints,
+    format: "esm",
+    metafile: true,
+    ...minifyOptions,
+    outdir: ".",
+    // This is requried to ensure the format of the outputFiles path is the same
+    // between windows and linux
+    absWorkingDir,
+    outfile: "",
+    platform: "neutral",
+    plugins: [denoPlugin({ importMapURL: importMapURL })],
+    sourcemap: dev ? "linked" : false,
+    splitting: true,
+    target: ["chrome99", "firefox99", "safari11", "safari15"],
+    treeShaking: true,
+    write: false,
+    jsx: JSX_RUNTIME_MODE[jsxConfig.jsx],
+    jsxImportSource: jsxConfig.jsxImportSource,
+  });
 
-    const absWorkingDir = Deno.cwd();
-    await ensureEsbuildInitialized();
-    // In dev-mode we skip identifier minification to be able to show proper
-    // component names in Preact DevTools instead of single characters.
-    const minifyOptions: Partial<BuildOptions> = this.#dev
-      ? { minifyIdentifiers: false, minifySyntax: true, minifyWhitespace: true }
-      : { minify: true };
-    const bundle = await esbuild.build({
-      bundle: true,
-      define: { __FRSH_BUILD_ID: `"${BUILD_ID}"` },
-      entryPoints,
-      format: "esm",
-      metafile: true,
-      ...minifyOptions,
-      outdir: ".",
-      // This is requried to ensure the format of the outputFiles path is the same
-      // between windows and linux
-      absWorkingDir,
-      outfile: "",
-      platform: "neutral",
-      plugins: [denoPlugin({ importMapURL: this.#importMapURL })],
-      sourcemap: this.#dev ? "linked" : false,
-      splitting: true,
-      target: ["chrome99", "firefox99", "safari11", "safari15"],
-      treeShaking: true,
-      write: false,
-      jsx: JSX_RUNTIME_MODE[this.#jsxConfig.jsx],
-      jsxImportSource: this.#jsxConfig.jsxImportSource,
-    });
-    // const metafileOutputs = bundle.metafile!.outputs;
+  return bundle;
+};
 
-    // for (const path in metafileOutputs) {
-    //   const meta = metafileOutputs[path];
-    //   const imports = meta.imports
-    //     .filter(({ kind }) => kind === "import-statement")
-    //     .map(({ path }) => `/${path}`);
-    //   this.#preloads.set(`/${path}`, imports);
-    // }
+const storeBundle = async (
+  bundle: BuildResult,
+  storage: BlobStorage,
+  absWorkingDir: string,
+) => {
+  await storage.clear();
 
-    const cache = new Map<string, Uint8Array>();
-    const absDirUrlLength = toFileUrl(absWorkingDir).href.length;
-    for (const file of bundle.outputFiles) {
-      cache.set(
+  const absDirUrlLength = toFileUrl(absWorkingDir).href.length;
+
+  await Promise.all([
+    bundle.outputFiles?.map((file) =>
+      storage.set(
         toFileUrl(file.path).href.substring(absDirUrlLength),
         file.contents,
-      );
-    }
-    this.#cache = cache;
+      )
+    ),
+  ]);
+};
 
-    return;
+export const createBundle = async (
+  options: Omit<Options, "absWorkingDir">,
+): Promise<BlobStorage> => {
+  const absWorkingDir = Deno.cwd();
+  const storagePath = join(absWorkingDir, "/.frsh");
+
+  if (options.dev) {
+    const fs = await fsStorage(storagePath);
+    const inMemory = inMemoryStorage();
+
+    const [prod, dev] = await Promise.all([
+      bundle({ ...options, dev: false, absWorkingDir }),
+      bundle({ ...options, dev: true, absWorkingDir }),
+    ]);
+
+    await Promise.all([
+      storeBundle(prod, fs, absWorkingDir),
+      storeBundle(dev, inMemory, absWorkingDir),
+    ]);
+
+    return inMemory;
+  } else {
+    return fsStorage(storagePath);
   }
-
-  async cache(): Promise<Map<string, Uint8Array>> {
-    if (this.#cache === undefined) {
-      this.#cache = this.bundle();
-    }
-    if (this.#cache instanceof Promise) {
-      await this.#cache;
-    }
-    return this.#cache as Map<string, Uint8Array>;
-  }
-
-  async get(path: string): Promise<Uint8Array | null> {
-    const cache = await this.cache();
-    return cache.get(path) ?? null;
-  }
-
-  // getPreloads(path: string): string[] {
-  //   return this.#preloads.get(path) ?? [];
-  // }
-}
+};
+  
