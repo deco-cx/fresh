11f2b2b92b7c97810b339002d3c65d2bb09a68d1
gimenes <tlgimenes@gmail.com>
2023-05-18 11:41:32 -0300

    add content-length to bundle

--- src/server/storage.ts
+++ src/server/storage.ts
@@ -5,16 +5,24 @@ export interface BlobStorage {
   set: (key: string, value: Uint8Array) => Promise<void>;
   get: (
     key: string,
-  ) => Promise<Uint8Array | ReadableStream<Uint8Array> | undefined>;
+  ) => Promise<
+    | { content: Uint8Array; size: number }
+    | { content: ReadableStream<Uint8Array>; size: number }
+    | undefined
+  >;
   clear: () => Promise<void>;
 }
 
 export const inMemoryStorage = (): BlobStorage => {
-  const map = new Map();
+  const map = new Map<string, Uint8Array>();
 
   return {
     clear: async () => map.clear(),
-    get: async (k: string) => map.get(k),
+    get: async (k: string) => {
+      const file = map.get(k);
+
+      return file && { content: file, size: file.length };
+    },
     set: async (k: string, v: Uint8Array) => {
       map.set(k, v);
     },
@@ -27,8 +35,17 @@ export const fsStorage = async (rootDir: string): Promise<BlobStorage> => {
   return {
     clear: () => emptyDir(rootDir),
     get: async (k: string) => {
-      const file = await Deno.open(join(rootDir, k), { read: true });
-      return file.readable;
+      try {
+        const path = join(rootDir, k);
+        const [stat, file] = await Promise.all([
+          Deno.lstat(path),
+          Deno.open(path, { read: true }),
+        ]);
+
+        return { content: file.readable, size: stat.size };
+      } catch {
+        return undefined;
+      }
     },
     set: (k: string, v: Uint8Array) =>
       Deno.writeFile(join(rootDir, k), v, { create: true, append: false }),
